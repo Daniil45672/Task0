8c5e6b7b7c185125c5b6cddf650d235aac8a4604
Daniil45672 <78366838+Daniil45672@users.noreply.github.com>
2021-06-13 19:11:03 +0300

    Add files via upload

---
+++ task0.py
@@ -0,0 +1,21 @@
+import random
+mas=[]
+mas2=[]
+num=0
+a=0
+for i in range(30):
+    mas.append(random.randint(-100,100))
+print(mas)
+for i in range(30):
+    if mas[i]==max(mas):
+        num=i
+print("Максимальний елемент:", max(mas))
+print("Індекс максимального элемента:", num)
+
+for i in range(30):
+    if (mas[i]%2)!=0:
+        mas2.append(mas[i])
+        a=1
+if a==0:
+    print("Немає непарних чисел")
+print("Список непарних чисел із начального списку:\n",sorted(mas2,reverse=True))
